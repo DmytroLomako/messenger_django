bcb47fd9d320537da9634574d53159f7709221cb
Mbarilo <mishapeters7@gmail.com>
2025-05-11 21:46:31 +0300

    emai verefication add

--- Messenger/Messenger/settings.py
+++ Messenger/Messenger/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 from django.urls import reverse_lazy
+from dotenv import load_dotenv
+import os
+import environ 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -134,9 +137,12 @@ LOGIN_REDIRECT_URL = reverse_lazy("main")
 
 
 # Email settings
+
+ENV_DIR = Path(__file__).resolve().parent.parent.parent 
+environ.Env.read_env(ENV_DIR / '.env')
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'quiz.tg.bot@gmail.com' # e-mail
-EMAIL_HOST_PASSWORD = 'fhyb lfxu rqpx sbmy' # e-mail app password 
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # e-mail
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD') # e-mail app password 
