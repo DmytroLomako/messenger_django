54854021bd5bff869e8c15d99a2224e7aed4766a
dimachep1408 <dmitriychep2011@gmail.com>
2025-05-11 21:27:03 +0300

    merge commit

--- Messenger/authorization/forms.py
+++ Messenger/authorization/forms.py
@@ -1,22 +1,28 @@
 from django import forms
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 class RegistrationForm(UserCreationForm):
     
     class Meta:
         model = User
-        fields = ['username', 'email', 'password1', 'password2']
+        fields = ['email', 'password1', 'password2']
         widgets = {
-            'email': forms.TextInput(attrs={"class": 'email', "placeholder": "email", 'required': True}),
+            'email': forms.TextInput(attrs={"class": 'email', "placeholder": "you@gmail.com", 'required': True}),
         }
         
     def __init__(self, *args, **kwargs):
         super(RegistrationForm, self).__init__(*args, **kwargs)
-        self.fields['username'].widget = forms.TextInput(attrs={"class": 'username', "placeholder": "username"})
+        self.fields['email'].label = "Електронна пошта"
         self.fields['password1'].widget = forms.PasswordInput(attrs={'class': 'password', "placeholder": "password"})
+        self.fields['password1'].label = "Пароль"
         self.fields['password2'].widget = forms.PasswordInput(attrs={'class': 'password', "placeholder": "confirm password"})
-    
+        self.fields['password2'].label = "Підтвердити пароль"
+        self.fields["password2"].help_text = None
+        self.fields["password1"].help_text = None
+        self.fields["email"].help_text = None
+
+
     def clean_email(self):
         email = self.cleaned_data.get('email')
         return email
@@ -28,3 +34,9 @@ class RegistrationForm(UserCreationForm):
         if commit:
             user.save()
         return user
+    
+
+
+class CustomLoginForm(AuthenticationForm):
+    username = forms.CharField(widget=forms.TextInput(attrs={"class": 'email', "placeholder": "you@example.com"}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'password', "placeholder": "Введи пароль"}))
